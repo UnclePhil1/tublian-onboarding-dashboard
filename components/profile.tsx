8045f6bddd52fb26dd621006a5a304a2b8d3957c
Unclephil <philipnssien@gmail.com>
2024-03-25 17:36:07 +0100

    making deploment and images

--- components/profile.tsx
+++ components/profile.tsx
@@ -5,7 +5,7 @@ const Profile = () => {
   return (
     <div className='flex justify-start items-start'>
         <Image 
-            src={'/Images/profile.jpg'}
+            src={'./Images/profile.jpg'}
             alt='profile'
             width={700}
             height={300}
